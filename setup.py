5b47b2351109514a5263ddf45d6caa8af1ae18bd
zotroneneis <zotroneneis@users.noreply.github.com>
2019-03-29 13:48:52 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+NAME = 'horseshoe_bnn'
+VERSION = '1.0'
+AUTHOR = 'Anna-Lena Popkes, Hiske Overweg'
+DESCRIPTION = 'Horseshoe BNN model for performing feature selection'
+PACKAGES = setuptools.find_packages()
+LICENSE = 'MIT LICENSE'
+LONG_DESCRIPTION = 'This package includes code for training and evaluating Baysian models for performing '\
+                   'feature selection. Four models are included: two (linear and non-linear) using '\
+                   'Gaussian prior distributions and two (linear and non-linear) using Horseshoe prior. '\
+                   'distributions. The horseshoe distribution introduces sparsity in the latter two models '\
+                   'and allows for performing feature selection.'
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    license=LICENSE,
+    long_description=LONG_DESCRIPTION,
+)
