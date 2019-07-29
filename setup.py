33d8b47fcba682ee8a491545878111b61d9c0cd5
Yordan Zaykov <35663766+yordanz@users.noreply.github.com>
2019-07-29 19:35:38 +0100

    Updated license in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ VERSION = '1.0'
 AUTHOR = 'Anna-Lena Popkes, Hiske Overweg'
 DESCRIPTION = 'Horseshoe BNN model for performing feature selection'
 PACKAGES = setuptools.find_packages()
-LICENSE = 'MIT LICENSE'
+LICENSE = 'MICROSOFT RESEARCH LICENSE'
 LONG_DESCRIPTION = 'This package includes code for training and evaluating Baysian models for performing '\
                    'feature selection. Four models are included: two (linear and non-linear) using '\
                    'Gaussian prior distributions and two (linear and non-linear) using Horseshoe prior. '\
